e299c29d31a6240ff1cbf474da545db9012f6d1b
apatil <apatil@ihmc.org>
2022-10-16 18:07:42 -0400

    WIP: Message handling.

---
+++ Messages/FilePiece.java
@@ -0,0 +1,60 @@
+package Messages;
+import Configurations.SystemConfiguration;
+import java.nio.ByteBuffer;
+
+public class FilePiece {
+    private int pieceIndex;
+    private byte[] content;
+    private String fromPeer;
+    private int isPieceAvailable;
+
+
+    public FilePiece() {
+        this.pieceIndex = -1;
+        this.content = new byte[SystemConfiguration.pieceSize];
+        this.fromPeer = null;
+        this.isPieceAvailable = 0;
+    }
+
+    public int getPieceIndex() {
+        return pieceIndex;
+    }
+
+    public void setPieceIndex(int pieceIndex) {
+        this.pieceIndex = pieceIndex;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public String getFromPeer() {
+        return fromPeer;
+    }
+
+    public void setFromPeer(String fromPeer) {
+        this.fromPeer = fromPeer;
+    }
+
+    public int getIsPieceAvailable() {
+        return isPieceAvailable;
+    }
+
+    public void setIsPieceAvailable(int isPieceAvailable) {
+        this.isPieceAvailable = isPieceAvailable;
+    }
+
+    public FilePiece getFilePieceFromPayload(byte[] payload) {
+        byte[] indexBytes = new byte[Constants.PIECE_INDEX_LENGTH];
+        FilePiece filePiece = new FilePiece();
+        System.arraycopy(payload, 0, indexBytes, 0, Constants.PIECE_INDEX_LENGTH);
+        filePiece.setPieceIndex(ByteBuffer.wrap(indexBytes).getInt());
+        filePiece.setContent(new byte[payload.length - Constants.PIECE_INDEX_LENGTH]);
+        System.arraycopy(payload, Constants.PIECE_INDEX_LENGTH, filePiece.getContent(), 0,  payload.length - Constants.PIECE_INDEX_LENGTH);
+        return filePiece;
+    }
+}
---
+++ Messages/MessageMetadata.java
@@ -0,0 +1,27 @@
+package Messages;
+
+public class MessageMetadata {
+    private Msg msg;
+    private String senderId;
+
+    public MessageMetadata() {
+        this.msg = new Msg();
+    }
+
+    public Msg getMsg() {
+        return msg;
+    }
+
+    public void setMsg(Msg msg) {
+        this.msg = msg;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+}
+
